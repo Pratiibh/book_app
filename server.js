d9f1e44d41e2ab92847c0348bf4930043ed7f880
Pratiibh <pratiibhbassi@gmail.com>
2019-04-30 07:32:23 -0700

    bugfixes and feature updates

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
-// const methodOverride = require('method-override');
+const methodOverride = require('method-override');
 
 // Application Setup
 const app = express();
@@ -17,23 +17,25 @@ const client = new pg.Client(process.env.DATABASE_URL);
 client.on('error', console.error);
 client.connect();
 
+// Application Middleware
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended: true}));
+
 // // look in the urlencoded POST body and delete _method then change to a put
-// app.use(methodOverride((request, response) => {
-//   if(request.body && typeof request.body === 'object' && '_method' in request.body) {
-//     let method = request.body._method; // 'PUT';
-//     delete request.body._method;
-//     return method; // 'PUT'
-//   }
-// }))
+app.use(methodOverride((request, response) => {
+  console.log(request.body);
+  if(request.body && typeof request.body === 'object' && '_method' in request.body) {
+    let method = request.body._method; // 'PUT';
+    delete request.body._method;
+    return method; // 'PUT'
+  }
+}))
 
 // SQL commands
 const SQL = {};
 SQL.getAllData = 'SELECT * FROM book_app;';
 SQL.idCheck = 'SELECT * FROM book_app WHERE id=$1;';
-
-// Application Middleware
-app.use(express.static('./public'));
-app.use(express.urlencoded({extended: true}));
+SQL.deleteBook = 'DELETE FROM book_app WHERE id=$1';
 
 // Set the view engine for server-side templating
 app.set('view-engine', 'ejs');
@@ -43,7 +45,7 @@ app.set('view-engine', 'ejs');
 app.get('/', (request, response) => {
   client.query(SQL.getAllData).then(result => {
     // console.log(testSelected);
-    console.log(result.rows)
+    // console.log(result.rows)
     response.render('pages/index.ejs', {testing:result.rows});
   })
 })
@@ -96,10 +98,21 @@ app.post('/searches', (request, response) => {
   // console.log(request.body);
 })
 
+app.delete('/details/:id', removeFromDB);
+
+
 // HELPER FUNCTIONS
 function build_book_display(val){
   let book_object = new Book_input(val)
   return book_object;
+}
+
+function removeFromDB(request, response){
+  console.log('====================================================')
+  const deletedBook = parseInt(request.params.id);
+  client.query(SQL.deleteBook, [deletedBook]).then(result => {
+    response.redirect('/');
+  })
 }
 
 // Book constructor
